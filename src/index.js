5aadb8ac1dd0f73d6d4884690b0fd0f5b6f0b8c1
Matteo Piccini <gnappo.1@live.it>
2024-02-01 17:13:39 -0800

    after walkthrough code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.querySelector('#create-task-form')
+  const description = document.querySelector('#new-task-description')
+  const ul = document.querySelector('#tasks')
+  
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const task = description.value;
+    const li = document.createElement('li');
+    li.innerText = task
+    //! create a button
+    const button = document.createElement('button')
+    //! give it an "x" as text
+    button.innerText = 'x'
+    button.addEventListener('click', () => li.remove())
+    // button.addEventListener('click', (e) => e.target.parentNode.remove())
+    li.appendChild(button)
+    ul.append(li)
+    e.target.reset()
+  }
+
+  form.addEventListener('submit', handleSubmit)
 });
